52419c8b9df6c26cf54a277f3cadfacf10ede470
jimenezzz <j.jimenez21@uniandes.edu.co>
2023-10-03 21:37:56 -0500

    added matches and notifications view

---
+++ lib/presentation/screens/matches_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'bottombar.dart';
+
+class MatchesView extends StatefulWidget {
+  const MatchesView({super.key});
+
+  @override
+  _MatchesViewState createState() => _MatchesViewState();
+}
+
+class _MatchesViewState extends State<MatchesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Matches view'),
+          backgroundColor: const Color.fromRGBO(170, 62, 152, 1),
+        ),
+        body: const Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        bottomNavigationBar: const BottomNavigationBarExample()
+    );
+  }
+}
---
+++ lib/presentation/screens/notifications_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'bottombar.dart';
+
+class NotificationsView extends StatefulWidget {
+  const NotificationsView({super.key});
+
+  @override
+  _NotificationsViewState createState() => _NotificationsViewState();
+}
+
+class _NotificationsViewState extends State<NotificationsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notifications view'),
+        backgroundColor: const Color.fromRGBO(170, 62, 152, 1),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+        bottomNavigationBar: const BottomNavigationBarExample()
+    );
+  }
+}
