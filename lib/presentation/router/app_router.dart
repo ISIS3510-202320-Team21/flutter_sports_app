b795736b61dcbd344307a0402014caa76e15ba19
jimenezzz <j.jimenez21@uniandes.edu.co>
2023-10-03 21:58:49 -0500

    new routes

--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_sports/presentation/screens/home_view.dart';
 import 'package:flutter_app_sports/presentation/screens/login_view.dart';
+import 'package:flutter_app_sports/presentation/screens/matches_view.dart';
+import 'package:flutter_app_sports/presentation/screens/notifications_view.dart';
 import 'package:flutter_app_sports/presentation/screens/signUp_view.dart';
 
 class AppRouter {
@@ -23,7 +25,14 @@ class AppRouter {
         return MaterialPageRoute(
           builder: (_) => const HomeView(),
         );
-
+      case '/notifications':
+        return MaterialPageRoute(
+          builder: (_) => const NotificationsView(),
+        );
+      case '/matches':
+        return MaterialPageRoute(
+          builder: (_) => const MatchesView(),
+        );
       default:
         return null;
     }
