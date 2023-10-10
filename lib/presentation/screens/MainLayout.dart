77a690e62938808dcc30dd4724f8dcd61edda1e1
Brian Rivera <rbrina4@gmail.com>
2023-10-09 22:28:15 -0500

    notifications connected

--- lib/presentation/screens/MainLayout.dart
+++ lib/presentation/screens/MainLayout.dart
@@ -4,13 +4,13 @@ import 'package:flutter_app_sports/logic/blocs/global_events/bloc/global_event.d
 import 'package:flutter_app_sports/logic/blocs/global_events/bloc/global_state.dart';
 import 'package:flutter_app_sports/presentation/screens/home_view.dart';
 import 'package:flutter_app_sports/presentation/screens/matches_view.dart';
+import 'package:flutter_app_sports/presentation/screens/notifications_view.dart';
 import 'package:flutter_app_sports/presentation/screens/profile_view.dart';
 import 'package:flutter_app_sports/presentation/widgets/CustomBottomNavigationBar.dart';
 import 'package:flutter_app_sports/presentation/widgets/SquareIconButton.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'notifications_view.dart';
 
 class MainLayout extends StatefulWidget {
   @override
@@ -18,11 +18,13 @@ class MainLayout extends StatefulWidget {
 }
 
 class _MainLayoutState extends State<MainLayout> {
-  int _selectedIndex = 0;
+  int _selectedIndex = -1;  // -1 indica que ningún ítem está seleccionado
+
   final List<String> _titles = [
     "HOME",
     "MATCHES",
-    "MY PROFILE"
+    "MY PROFILE",
+    "NOTIFICATIONS",
   ];
 
   @override
@@ -33,16 +35,17 @@ class _MainLayoutState extends State<MainLayout> {
 
     return BlocBuilder<GlobalBloc, GlobalState>(
       builder: (context, state) {
-        if (state is NavigationState) {
+        if (state is NavigationStateButtons) {
           _selectedIndex = state.selectedIndex;
         }
+
         return Scaffold(
           appBar: AppBar(
             automaticallyImplyLeading: false,
             backgroundColor: colorScheme.onPrimary,
             elevation: 0.0,
             title: Text(
-              _titles[_selectedIndex],
+              _titles[_selectedIndex >= 0 ? _selectedIndex : 3],  // Usar el índice 3 (NOTIFICATIONS) si ningún ítem está seleccionado
               style: textTheme.headlineSmall?.copyWith(
                 color: colorScheme.onBackground,
                 fontWeight: FontWeight.bold,
@@ -59,21 +62,23 @@ class _MainLayoutState extends State<MainLayout> {
                   // Acción para el ícono de mensajes
                 },
               ),
-              SquareIconButton(
-                iconData: Icons.notifications,
-                onPressed: () {
-                  // Acción para el ícono de notificaciones
-                  Navigator.of(context).pushNamed('/notifications');
-                },
-              ),
+              if (_selectedIndex != 3)  // Mostrar el ícono de notificaciones solo si no estamos en la pantalla de notificaciones
+                SquareIconButton(
+                  iconData: Icons.notifications,
+                  onPressed: () {
+                    BlocProvider.of<GlobalBloc>(context)
+                        .add(NavigateToIndexEvent(3));  
+                  },
+                ),
             ],
           ),
           body: IndexedStack(
-            index: _selectedIndex,
+            index: _selectedIndex >= 0 ? _selectedIndex : 3,  // Usar el índice 3 (NOTIFICATIONS) si ningún ítem está seleccionado
             children: const [
               HomeView(),
               MatchesView(),
-              ProfileView()
+              ProfileView(),
+              NotificationsView(),
             ],
           ),
           bottomNavigationBar: CustomBottomNavigationBar(
--- lib/presentation/widgets/CustomBottomNavigationBar.dart
+++ lib/presentation/widgets/CustomBottomNavigationBar.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
-  final int selectedIndex;
+  final int? selectedIndex;
   final Function(int) onItemTapped;
 
-  const CustomBottomNavigationBar({super.key, 
+  const CustomBottomNavigationBar({
     required this.selectedIndex,
     required this.onItemTapped,
   });
@@ -13,29 +13,71 @@ class CustomBottomNavigationBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-    return BottomNavigationBar(
-      elevation: 0.0,
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home, color: colorScheme.onBackground),
-          activeIcon: Icon(Icons.home, color: colorScheme.primary),
-          label: 'Home',
+    return Container(
+      height: 70,  
+      decoration: BoxDecoration(
+        color: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+        border: Border(
+          top: BorderSide(
+            color: colorScheme.onBackground.withOpacity(0.2),  
+            width: 1.0,
+          ),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.fast_forward, color: colorScheme.onBackground),
-          activeIcon: Icon(Icons.fast_forward, color: colorScheme.primary),
-          label: 'Matches',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person, color: colorScheme.onBackground),
-          activeIcon: Icon(Icons.person, color: colorScheme.primary),
-          label: 'Profile',
-        ),
-      ],
-      currentIndex: selectedIndex,
-      selectedItemColor: colorScheme.primary,
-      unselectedItemColor: colorScheme.onBackground,
-      onTap: onItemTapped,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: List.generate(3, (index) {
+          final isSelected = index == selectedIndex;
+          return InkWell(
+            borderRadius: BorderRadius.circular(100),  
+            onTap: () => onItemTapped(index),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),  
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    getIconData(index),
+                    color: isSelected ? colorScheme.primary : colorScheme.onBackground,
+                  ),
+                  Text(
+                    getLabel(index),
+                    style: TextStyle(
+                      color: isSelected ? colorScheme.primary : colorScheme.onBackground,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
     );
+  }
+
+  IconData getIconData(int index) {
+    switch (index) {
+      case 0:
+        return Icons.home;
+      case 1:
+        return Icons.fast_forward;
+      case 2:
+        return Icons.person;
+      default:
+        throw Exception('Invalid index');
+    }
+  }
+
+  String getLabel(int index) {
+    switch (index) {
+      case 0:
+        return 'Home';
+      case 1:
+        return 'Matches';
+      case 2:
+        return 'Profile';
+      default:
+        throw Exception('Invalid index');
+    }
   }
 }
