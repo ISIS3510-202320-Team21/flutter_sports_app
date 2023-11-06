12d7ffec66c35a5eacfd6b23c2aa614fe2eeb65b
JuanSH11 <js.hoyosm@uniandes.edu.co>
2023-11-06 17:57:35 -0500

    Add user image to profile view

--- lib/presentation/screens/profile_view.dart
+++ lib/presentation/screens/profile_view.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app_sports/logic/blocs/profile/profile_bloc.dart';
@@ -10,6 +11,7 @@ import 'package:restart_app/restart_app.dart';
 import '../../logic/blocs/authentication/bloc/authentication_bloc.dart';
 import '../../logic/blocs/global_events/bloc/global_bloc.dart';
 import '../../logic/blocs/global_events/bloc/global_event.dart';
+import 'dart:convert';
 
 class ProfileView extends StatefulWidget {
   const ProfileView({Key? key}) : super(key: key);
@@ -22,9 +24,12 @@ class _ProfileViewState extends State<ProfileView> {
   final ProfileBloc _profileBloc = ProfileBloc();
 
   String? userName;
+  String? userImage;
+
   void initState() {
     super.initState();
     userName = BlocProvider.of<AuthenticationBloc>(context).user?.name;
+    userImage = BlocProvider.of<AuthenticationBloc>(context).user!.imageUrl;
   }
 
   @override
@@ -46,10 +51,17 @@ class _ProfileViewState extends State<ProfileView> {
         }
       },
       builder: (context, state) {
-        if (state is ProfileLoadedSuccessState) {
-          // Si el estado es ProfileLoadedSuccessState, muestra la imagen de perfil
-          final profileImagePath = state.profileImagePath;
+        // Convierte la cadena Base64 en bytes
+          String base64String = "data:image/png;base64,${userImage!}";
 
+          // Elimina el encabezado
+          String base64Content = base64String.split(',').last;
+          Uint8List bytes = base64.decode(base64Content);
+          Image image = Image.memory(
+            bytes,
+            width: 80.0,
+            height: 80.0,
+          );
           return Scaffold(
             body: Stack(
               children: [
@@ -73,12 +85,8 @@ class _ProfileViewState extends State<ProfileView> {
                             backgroundColor: colorScheme.background,
                             shadowColor: Colors.transparent,
                           ),
-                          child: profileImagePath != null
-                              ? Image.file(
-                                  File(profileImagePath),
-                                  width: 80.0,
-                                  height: 80.0,
-                                )
+                          child: userImage != null
+                              ? image
                               : const Icon(
                                   Icons.account_circle,
                                   size: 80.0,
@@ -140,95 +148,9 @@ class _ProfileViewState extends State<ProfileView> {
               ],
             ),
           );
-        } else {
           // Si no estás en ProfileLoadedSuccessState, muestra el icono de perfil por defecto.
-          return Scaffold(
-            body: Stack(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            // Acción cuando se presiona el botón de perfil
-                            _profileBloc.add(
-                              ProfileAddProfilePictureButtonClickedEvent(),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            shape: const CircleBorder(),
-                            padding: const EdgeInsets.all(16.0),
-                            backgroundColor: colorScheme.background,
-                            shadowColor: Colors.transparent,
-                          ),
-                          child: const Icon(
-                            Icons.account_circle,
-                            size: 80.0,
-                            color: Colors.black87,
-                          ),
-                        ),
-                      const SizedBox(
-                          height:
-                              20), // Espacio reducido entre el botón de perfil y el texto
-                      Text(
-                        userName != null ? '$userName' : 'User',
-                        style: textTheme.titleLarge!.copyWith(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Lato',
-                          color: colorScheme.primary,
-                        ),
-                      ),
-                      const SizedBox(
-                          height:
-                              40.0), // Espacio entre el nombre y los botones
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            _iconButtonWithText(
-                              Icons.edit,
-                              'Edit my profile',
-                              () {
-                                goToEditProfile();
-                              },
-                            ),
-                            const SizedBox(
-                                height: 16.0), // Espacio entre los botones
-                            _iconButtonWithText(
-                              Icons.settings,
-                              'Settings',
-                              () {
-                                // Acción cuando se presiona el botón
-                              },
-                            ),
-                            const SizedBox(
-                                height: 16.0), // Espacio entre los botones
-                            _iconButtonWithText(
-                              Icons.logout,
-                              'Log out',
-                              () {
-                                Restart.restartApp(webOrigin: '/');
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
         }
-      },
-    );
+    );  
   }
 
   Widget _iconButtonWithText(
