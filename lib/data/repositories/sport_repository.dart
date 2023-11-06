76a6291ad473ba45a0558f4d7657ed9f015fa804
Brian Rivera <rbrina4@gmail.com>
2023-11-05 19:17:04 -0500

    Add user sports storage functionality

--- lib/data/repositories/sport_repository.dart
+++ lib/data/repositories/sport_repository.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter_app_sports/data/models/sport.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_app_sports/data/services/config_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SportRepository {
   final String? backendUrl = ConfigService.backendUrl;
@@ -24,6 +25,24 @@ class SportRepository {
       throw Exception('Failed to fetch sports: ${response.statusCode}');
     }
   }
+
+  Future<void> saveSports(List<Sport> sports) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String sportsJson =
+        jsonEncode(sports.map((sport) => sport.toJson()).toList());
+    await prefs.setString('sports_matches', sportsJson);
+  }
+
+  Future<List<Sport>?> fetchSportsStorage() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? sportsJson = prefs.getString('sports_matches');
+    if (sportsJson == null) {
+      return [];
+    }
+    List<dynamic> sportsList = jsonDecode(sportsJson) as List;
+    return sportsList.map((json) => Sport.fromJson(json)).toList();
+  }
+
   Future<Sport?> getSport({required int sportId}) async {
     final response = await http.get(
       Uri.parse('$backendUrl/sports/'),
